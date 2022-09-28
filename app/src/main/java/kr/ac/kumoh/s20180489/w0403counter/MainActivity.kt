a2087e6361f230fda778c58b241f041aca25b4db
qkrwnstlr <akfldkskgorn@gmail.com>
2022-09-28 15:29:29 +0900

    :rocket: 2022-09-28 Compose Counter

--- app/src/main/java/kr/ac/kumoh/s20180489/w0403counter/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20180489/w0403counter/MainActivity.kt
@@ -1,43 +1,94 @@
 package kr.ac.kumoh.s20180489.w0403counter
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import kr.ac.kumoh.s20180489.w0403counter.ui.theme.W0403CounterTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            W0403CounterTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+            MyApp {
+                Counter()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MyApp(content: @Composable () -> Unit) {
+    W0403CounterTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.background
+        ) {
+            content()
+        }
+    }
+}
+
+@Composable
+fun Counter() {
+    var count by remember { mutableStateOf(0) }
+    Column(
+        modifier = Modifier
+            .padding(8.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(
+            text = "$count",
+            fontSize = 70.sp,
+        )
+        Row {
+            Button(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f),
+                onClick = {
+                    count++
+                }
+            ) {
+                Text("증가")
+            }
+            Spacer(modifier = Modifier.width(8.dp))
+            Button(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f),
+                onClick = {
+                    count--
+                    if(count < 0) count = 0
+                }
+            ) {
+                Text("감소")
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    W0403CounterTheme {
-        Greeting("Android")
+    MyApp {
+        Column {
+            Counter()
+            Counter()
+            Counter()
+        }
     }
 }
